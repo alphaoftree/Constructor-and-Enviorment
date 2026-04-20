eb9abf5758cc3324f714f02e1ee24af86f2d4912
alphaoftree <52880749+alphaoftree@users.noreply.github.com>
2026-04-20 10:14:50 +0800

    Revert "feat: 建筑地形Demo（BuildDemo）"
    This reverts commit 6f3d905b07224fa286d7457d18bb9cbbc0e61b89.

--- src/demo-entry.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Demo from './BuildDemo'
-
-ReactDOM.createRoot(document.getElementById('root')).render(<Demo />)
